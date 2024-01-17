f65e8302abf215bfc9bc16278f487aae72ecf982
chinteng23 <chinteng.22@ichat.sp.edu.sg>
2024-01-17 15:15:00 +0800

    Added profile and settings button, search bar

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Calendar;
+import androidx.appcompat.widget.SearchView;
+
 
 public class MainActivity extends AppCompatActivity{
 
@@ -33,5 +37,16 @@ public class MainActivity extends AppCompatActivity{
         } else {
             greetingTextView.setText("Good Evening\nJohn.");
         }
+
+        SearchView searchView = findViewById(R.id.searchView);
+
+        // Set OnClickListener for the entire SearchView
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event here
+                // You can open a search activity, show suggestions, etc.
+            }
+        });
     }
 }
