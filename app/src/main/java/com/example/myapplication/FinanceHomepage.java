f4d49c321581044f95a04010aa5c880382787beb
Coderman556 <wilmer.ng.5@gmail.com>
2024-01-28 21:42:26 +0800

    ADD:getDisplayableSnB() in DBHandler.java;INPROG:styling FinanceHomepage;

--- app/src/main/java/com/example/myapplication/FinanceHomepage.java
+++ app/src/main/java/com/example/myapplication/FinanceHomepage.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -13,30 +17,30 @@ public class FinanceHomepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finance_homepage);
         DBHandler db = new DBHandler(this);
-        List<FinanceSnB> SnBList = db.getAllSnB();
+        List<FinanceSnB> SnBList = db.getDisplayableSnB();
 
         for (FinanceSnB snb : SnBList) {
-//            TODO: Investigate creating a break condition to stop iterating through saving/budget
+//            TODO: inflate CardView/Button with relevant data.
             if (snb.getGoalType().equals("saving")) {
-                if (snb.getGoalPeriod().equals("daily")) {
-//                    TODO: Inflate CardView with daily data
-                } else if (snb.getGoalPeriod().equals("weekly")) {
-//                    TODO: Inflate CardView with weekly data
-                } else if (snb.getGoalPeriod().equals("monthly")) {
-//                    TODO: Inflate CardView with monthly data
-                } else {
-//                    TODO: Display Button to add a Savings Goal
-                }
-            } else {
-                if (snb.getGoalPeriod().equals("daily")) {
-//                    TODO: Inflate CardView with daily data
-                } else if (snb.getGoalPeriod().equals("weekly")) {
-//                    TODO: Inflate CardView with weekly data
-                } else if (snb.getGoalPeriod().equals("monthly")) {
-//                    TODO: Inflate CardView with monthly data
-                } else {
-//                    TODO: Display Button to add a Savings Goal
-                }
+                Button button = findViewById(R.id.button_savings_goal);
+                button.setVisibility(View.GONE);
+
+                CardView cardview = findViewById(R.id.cvSavings);
+                cardview.setVisibility(View.VISIBLE);
+
+                TextView title = findViewById(R.id.txtSavings);
+                title.setText(snb.getGoalPeriod() + " " + snb.getGoalType());
+            }
+            if (snb.getGoalType().equals("budget")){
+                Button button = findViewById(R.id.button_savings_goal);
+                button.setVisibility(View.GONE);
+
+                CardView cardview = findViewById(R.id.cvBudget);
+                cardview.setVisibility(View.VISIBLE);
+//                TODO: change the data displayed programmatically depending on what the get method returns.
+
+                TextView title = findViewById(R.id.txtBudget);
+                title.setText(snb.getGoalPeriod() + " " + snb.getGoalType());
             }
         }
     }
